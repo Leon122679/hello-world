669a13121b137006e66b5d07003e3b65c7547db7
xlxmc <xlxmc@mail.missouri.edu>
2018-02-05 14:31:39 -0600

    your message Part1

---
+++ Lab1.1.c
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : 1.c
+ Author      : Leon
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#define pi 3.14
+
+#include <stdio.h>
+
+void main()
+{
+	float r,area;	//define the variables
+
+	printf("Please enter the R:");
+
+	scanf("%f",&r);	//input the radius
+
+	area=pi*r*r;	//calculate the area
+
+	printf("The area of the circle is: %.2f",area);	//out put the area.
+}
+
